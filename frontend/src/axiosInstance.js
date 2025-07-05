097ffb02fe2e6e255c826d7108992a2193c661d1
mohhd-salman <mohhd.salman@gmail.com>
2025-07-04 19:13:30 -0700

    [UPDATE] backend URL

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://community-pulse.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
